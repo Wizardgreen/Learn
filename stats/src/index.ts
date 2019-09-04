e5b5d1a8fcf9b371ed65fa3f6ab61e47c127391f
Greene <t0937383@gmail.com>
2019-09-04 14:32:08 +0800

    Add HtmlReport for Project stats

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -3,14 +3,12 @@ import { CsvFileReader } from "./CsvFileReader";
 import { WinsAnalysis } from "./analyzer/WinsAnalysis";
 import { ConsoleReport } from "./report/ConsoleReport";
 import { Summary } from "./Summary";
+import { HtmlReport } from "./report/HtmlReport";
 const csvFileReader = new CsvFileReader("stats/football.csv");
 
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
 
-const summary = new Summary(
-  new WinsAnalysis("Man United"),
-  new ConsoleReport()
-);
+const summary = new Summary(new WinsAnalysis("Man United"), new HtmlReport());
 
 summary.buildAndOutputReport(matchReader.matches);
