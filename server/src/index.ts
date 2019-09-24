ec7df005744113096adc6b0f2d34fccb7605881f
Greene <t0937383@gmail.com>
2019-09-24 16:53:43 +0800

    feat: basd login/out function

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,12 @@
-console.log("hi Greene");
+import express from "express";
+import bodyParser from "body-parser";
+import cookieSession from "cookie-session";
+import { router } from "./routes/loginRoutes";
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieSession({ keys: ["asflj"] }));
+app.use(router);
+
+app.listen(3000, () => console.log("listing 3000"));
---
+++ server/src/routes/loginRoutes.ts
@@ -0,0 +1,59 @@
+import { Router, Request, Response } from "express";
+
+interface RequestWithBody extends Request {
+  body: { [key: string]: string | undefined };
+}
+
+const router = Router();
+
+router.get("/", (req: Request, res: Response) => {
+  if (req.session && req.session.loggedIn) {
+    res.send(`
+      <div>
+        <div>You are logged in.</div>
+        <a href="/logout">Logout</a>
+      </div>
+    `);
+  } else {
+    res.send(`
+      <div>
+        <div>You are not logged in.</div>
+        <a href="/login">Login</a>
+      </div>
+    `);
+  }
+});
+
+router.get("/login", (req: Request, res: Response) => {
+  res.send(`
+  <form method="POST">
+    <div>
+      <label>Email</label>
+      <input name="email" value="hi@hi.com"/>
+    </div>
+    <div>
+      <label>Password</label>
+      <input name="password" type="password" value="password"/>
+    </div>
+    <button>submit</button>
+  </form>
+  `);
+});
+
+router.post("/login", (req: RequestWithBody, res: Response) => {
+  const { email, password } = req.body;
+
+  if (email && password && email === "hi@hi.com" && password === "password") {
+    req.session = { loggedIn: true };
+    res.redirect("/");
+  } else {
+    res.send("Invalid email or password");
+  }
+});
+
+router.get("/logout", (req: Request, res: Response) => {
+  req.session = undefined;
+  res.redirect("/");
+});
+
+export { router };
