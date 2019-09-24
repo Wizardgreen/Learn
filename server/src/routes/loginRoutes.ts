a760c02d9d7538efbb661495242c744e5fdad8ee
Greene <t0937383@gmail.com>
2019-09-24 17:19:39 +0800

    feat: add auth middleware

--- server/src/routes/loginRoutes.ts
+++ server/src/routes/loginRoutes.ts
@@ -1,7 +1,16 @@
-import { Router, Request, Response } from "express";
+import { Router, Request, Response, NextFunction } from "express";
 
 interface RequestWithBody extends Request {
   body: { [key: string]: string | undefined };
+}
+
+function requireAuth(req: Request, res: Response, next: NextFunction): void {
+  if (req.session && req.session.loggedIn) {
+    next();
+    return;
+  }
+  res.status(403);
+  res.send("Not permitted");
 }
 
 const router = Router();
@@ -54,6 +63,10 @@ router.post("/login", (req: RequestWithBody, res: Response) => {
 router.get("/logout", (req: Request, res: Response) => {
   req.session = undefined;
   res.redirect("/");
+});
+
+router.get("/protected", requireAuth, (req: Request, res: Response) => {
+  res.send("welcome!");
 });
 
 export { router };
